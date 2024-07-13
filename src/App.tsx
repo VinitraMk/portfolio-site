7cc53ae76ce0435481de62fc4b5278b86fbc7e0c
Vinitra Muralikrishnan <vinitramk@gmail.com>
2024-07-13 05:29:55 -0400

    initializing home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import './assets/scss/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+        <header className="portfolio-header">
+            <div className="portfolio-header__content">
+                <div>
+                    <div className="portfolio-header__fname">Vinitra</div>
+                    <div className="portfolio-header__lname">Muralikrishnan</div>
+                </div>
+                <div>
+                    <a className='portfolio-header__btn-link'>About</a>
+                </div>
+            </div>
+        {/*<img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+            Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+            Learn React
+        </a>*/}
+        </header>
+        <main className="portfolio-main">
+
+        </main>
+    </>
   );
 }
 
